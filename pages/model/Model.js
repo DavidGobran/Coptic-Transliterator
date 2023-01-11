fbfa11f520ef4eafb51bf2cc41adb1cfa45408c5
DavidGobran <93219495+DavidGobran@users.noreply.github.com>
2023-01-11 16:40:00 -0500

    finished most transliteration, crude dashes

--- pages/model/Model.js
+++ pages/model/Model.js
@@ -12,7 +12,7 @@ export default class Model {
             const prev = lowercase[index - 1]
             const next = lowercase[index + 1]
             switch (true) {
-                // ignore special characters
+                // ignore jenkim, already handled
                 case item === '̀':
                     return
                 // add jenkim to consonant
@@ -21,40 +21,43 @@ export default class Model {
                     break
                 // don't add jenkim to vowel
                 case (vowels.has(item) && next === '̀'):
-                    // lowercase[index + 1] = item
                     break    
-                // TODO: handle names
-
-                // TODO: handle ⲏⲓ
-
-                /* ⲩ cases */
+                // TODO: finish handling names
+                case (item === 'ⲓ' && next === 'ⲱ'):
+                    this.isUppercase(original[index]) ? res.push('Y') : res.push('y')
+                    return
+                /* special vowel cases */
+                // TODO: ⲟⲓ case, requies Greek distinction
+                // ⲏⲓ case
+                case (item === 'ⲏ' && next === 'ⲓ'):
+                    res.push('e')
+                    return
                 // ⲩ follows ⲁ or ⲉ
                 case ((item === 'ⲁ' || item === 'ⲉ') && next === 'ⲩ'):
-                    original[index] === original[index].toUpperCase() ? res.push(vowels.get(item).toUpperCase()) : res.push(vowels.get(item))
+                    this.isUppercase(original[index]) ? res.push(vowels.get(item).toUpperCase()) : res.push(vowels.get(item))
                     res.push('v')
                     return
                 // ⲩ follows ⲟ
                 case (item === 'ⲟ' && next === 'ⲩ'):
-                    original[index] === original[index].toUpperCase() ? res.push('O') : res.push('o')
+                    this.isUppercase(original[index]) ? res.push('O') : res.push('o')
                     res.push('u')
                     return
                 // ⲩ is follows ⲁ or ⲉ or ⲟ
                 case (item === 'ⲩ' && (prev === 'ⲁ' || prev === 'ⲉ' || prev === 'ⲟ')):
                     return
-                /* consonant cases */
+
+                /* special consonant cases */
                 // ⲅ makes g sound
-                case (item === 'ⲅ' && next === 'ⲓ'):
-                    original[index] === original[index].toUpperCase() ? res.push('G') : res.push('g')
+                case (item === 'ⲅ' && (next === 'ⲓ' || next === 'ⲉ')):
+                    this.isUppercase(original[index]) ? res.push('G') : res.push('g')
                     return
                 // consecutive ⲅ
                 case (item === 'ⲅ' && next === 'ⲅ'):
-                    res.push('ng')
-                    return
-                case (item === 'ⲅ' && prev === 'ⲅ'):
+                    res.push('n')
                     return
                 // ϫ makes g sound
                 case (item === 'ϫ' && (next === 'ⲁ' || next === 'ⲟ' || next === 'ⲱ')):
-                    original[index] === original[index].toUpperCase() ? res.push('G') : res.push('g')
+                    this.isUppercase(original[index]) ? res.push('G') : res.push('g')
                     return
                 // ⲃ at the end of word
                 case (item === 'ⲃ' && (!consonants.has(next) && !vowels.has(next))):
@@ -62,26 +65,36 @@ export default class Model {
                     return
                 // ⲭ makes sh sound
                 case (item === 'ⲭ' && (next === 'ⲉ' || next === 'ⲏ')):
-                    original[index] === original[index].toUpperCase() ? res.push('Sh') : res.push('sh')
+                    this.isUppercase(original[index]) ? res.push('Sh') : res.push('sh')
                     return
                 default:
                     console.log(item)
             }
             // preserve capitalization
-            if (original[index] === original[index].toUpperCase())
+            if (this.isUppercase(original[index]))
                 res.push(vowels.get(original[index]) || consonants.get(original[index]) || item)
             else
                 res.push(vowels.get(item) || consonants.get(item) || item)
         });
 
-        // TODO: add dashes for consecutive vowels
+        // add dashes for consecutive vowels
         res.forEach((item, index) => {
             const next = res[index + 1]
-            if (item.match(/\p{Letter}/gu) && (item === next || (item === 'u' && next === 'o')
-                                                                || (item === 'i' && next === 'e')
-                                                                || (item === 'a' && next === 'e')))
+            if (item.match(/\p{Letter}/gu) && ((item === next && this.isVowel(item)) 
+                                                || (item === 'u' && next === 'o')
+                                                || (item === 'i' && next === 'e')
+                                                || (item === 'a' && next === 'e')))
                 res[index] = item + '-'
         })
         return res.join('')
+    }
+
+    /* helper functions */
+    isVowel(char) {
+        return char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'
+    }
+
+    isUppercase(char) {
+        return char === char.toUpperCase()
     }
 }
