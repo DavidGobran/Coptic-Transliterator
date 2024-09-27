dee72a8c0939020c27d32cae3edbc3bc76b81f62
DavidGobran <93219495+DavidGobran@users.noreply.github.com>
2024-09-27 17:26:10 -0400

    Add metadata

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Coptic Transliterator</title>
+        <meta name="description" content="Automatically transliterate Coptic to English."/>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"></link>
       </Head>
       <body>
